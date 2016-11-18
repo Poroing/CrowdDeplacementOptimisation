537e42004e6bb3762c34e6a1e47977af9185b938
Poroing <jean-18-09@hotmail.fr>
2016-11-18 17:21:31 +0100

    Obstacle Rectangulaire

--- Simulation/src/obstacle_rectangulaire.py
+++ Simulation/src/obstacle_rectangulaire.py
@@ -1,35 +1,14 @@
 from pymunk.vec2d import Vec2d
 import pymunk
 
-
 class ObstacleRectangulaire (object):
+
     def __init__ (self, hauteur, largeur, position):
-        self.hauteur = hauteur
-        self.largeur = largeur
-        self.position = position
+
+        self.corps = pymunk.Body(body_type=pymunk.Body.STATIC)
+        self.corps.position = position
+
+        self.representation = pymunk.Poly.create_box(self.corps, size=(largeur, hauteur))
         
     def aujouterDansEspace(self, espace):
-        
-        sommet_bas_gauche = self.position
-        sommet_bas_droit = self.position + Vec2d(self.largeur, 0)
-        sommet_haut_droit = self.position + Vec2d(self.largeur, self.hauteur)
-        sommet_haut_gauche = self.position + Vec2d(0, self.hauteur)
-        
-        corps_mur_haut = pymunk.Body(body_type=pymunk.Body.STATIC)
-        mur_haut = pymunk.Segment(corps_mur_haut, sommet_haut_gauche, sommet_haut_droit, 0.0)
-        espace.add(corps_mur_haut, mur_haut)
-
-        corps_mur_gauche = pymunk.Body(body_type=pymunk.Body.STATIC)
-        mur_gauche = pymunk.Segment(corps_mur_gauche, sommet_haut_gauche, sommet_bas_gauche, 0.0)
-        espace.add(corps_mur_gauche, mur_gauche)
-
-        corps_mur_droit = pymunk.Body(body_type=pymunk.Body.STATIC)
-        mur_droit = pymunk.Segment(corps_mur_droit, sommet_bas_droit, sommet_haut_droit, 0.0)
-        espace.add(corps_mur_droit, mur_droit)
-
-        corps_mur_bas = pymunk.Body(body_type=pymunk.Body.STATIC)
-        mur_bas = pymunk.Segment(corps_mur_ba, sommet_bas_gauche, sommet_bas_droit, 0.0)
-        espace.add(corps_mur_bas, mur_bas)
-        
-        
-        
+        espace.add(self.corps, self.representation)
