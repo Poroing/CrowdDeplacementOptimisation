a1cc18b1f499e81d15f3cea20d37f03ef9065d6e
Crousthybalt <thibault.marette@gmail.com>
2017-04-07 20:36:39 +0500

    optiporte 2.0

--- ModelisationPhysique/src/FichiersTests/optiporte.py
+++ ModelisationPhysique/src/FichiersTests/optiporte.py
@@ -5,32 +5,33 @@ from traitement import RecuperationDeDonnees, TraitementDeDonnees
 import matplotlib.pyplot as plt
 from source_personne import Source
 from affichage import Afficheur
+from matplotlib.pyplot as plt
+
 
 afficheur = Afficheur()
 
 configuration = convertirJsonPython('../FichiersConfiguration/configuration_MPSI2_obstacle_devant_porte.json')
-recuperation = RecuperationDeDonnees(configuration, temps_maximal=20, action_mise_a_jour_secondaire=afficheur.dessinerEspaceEtAttendre)
 #recuperation.simulation.sources.append(Source(recuperation.simulation.espace, Vec2d(500, 750), 0.5))
 
-def optimiser_porte_debit(taille_min, taille_max, n ):
+taille_min, taille_max, n = 0 , 5 * configuration['personnes']['rayon_max'], 10
     
-    debit, taille_porte = [], []
+debit, taille_porte = [], []
     
-    for k in range (n):
+for k in range (n):
         
+
+    recuperation = RecuperationDeDonnees(configuration, temps_maximal=10, action_mise_a_jour_secondaire=afficheur.dessinerEspaceEtAttendre)
         
+    configuration['lieu_ferme']['porte_largeur'] = taille_min + k*(taille_max - taille_min)
         
-        configuration['lieu_ferme']['porte_largeur'] = taille_min + k*(taille_max - taille_min)
+    recuperation.lancer()
         
-        recuperation.lancer()
+    traitement = TraitementDeDonnees(recuperation.temps_de_sortie)
         
-        traitement = TraitementDeDonnees(recuperation.temps_de_sortie)
+    debit.append (traitement.avoirDebitMoyen())
         
-        debit.append (traitement.avoirDebitMoyen())
-        
-        taille_porte.append(taille_min + k*(taille_max - taille_min))
+    taille_porte.append(taille_min + k*(taille_max - taille_min))
         
             
-        
-    return taille_porte, debit
+plt.plot(debit, taille_porte)
 
